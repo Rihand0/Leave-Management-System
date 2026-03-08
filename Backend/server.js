9f5b0bd0ada7c33bdaa606985d66b4b320fb3172
Rihand0 <rihandminz432@gmail.com>
2026-03-08 14:19:59 +0530

    Add files via upload

---
+++ Backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const authRoutes = require("./routes/authRoutes");
+
+app.use("/api/auth", authRoutes);
+
+const leaveRoutes = require("./routes/leaveRoutes");
+
+app.use("/api/leave", leaveRoutes);
